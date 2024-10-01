5c48a0f4fbd3a3beb4ca9553443bd22b90e8a738
Dean @ Skool <dbjones3@students.wcpss.net>
2024-10-01 09:39:12 -0400

    errors + supabase

--- src/actions/auth/getUser.ts
+++ src/actions/auth/getUser.ts
@@ -2,13 +2,22 @@
 
 import type { User } from "@supabase/supabase-js"
 import prisma from "~/lib/prisma/client"
+import { createClient } from "~/lib/supabase/server"
 
 export async function getUserData(user: User) {
-	const data = await prisma.user.findUnique({
-		where: {
-			id: user.id
-		}
-	})
+    const supabase = createClient()
 
-	return data
+    const { data, error } = await supabase.auth.getUser(user.id)
+
+    if (error || !data.user) {
+        throw new Error("User not found or unauthorized")
+    }
+
+    const userData = await prisma.user.findUnique({
+        where: {
+            id: user.id
+        }
+    })
+
+    return userData
 }
