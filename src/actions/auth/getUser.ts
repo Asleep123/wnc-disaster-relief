df80bfb23607805052a7b59fc9d645f31cae8bd9
Dean @ Skool <dbjones3@students.wcpss.net>
2024-10-01 09:40:56 -0400

    i forgor why i renamed this

--- src/actions/auth/getUser.ts
+++ src/actions/auth/getUser.ts
@@ -7,17 +7,17 @@ import { createClient } from "~/lib/supabase/server"
 export async function getUserData(user: User) {
     const supabase = createClient()
 
-    const { data, error } = await supabase.auth.getUser(user.id)
+    const { data: supabaseUser, error } = await supabase.auth.getUser(user.id)
 
-    if (error || !data.user) {
+    if (error || !supabaseUser) {
         throw new Error("User not found or unauthorized")
     }
 
-    const userData = await prisma.user.findUnique({
+    const data = await prisma.user.findUnique({
         where: {
             id: user.id
         }
     })
 
-    return userData
+    return data
 }
